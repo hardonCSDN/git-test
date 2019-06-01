083d10320e52932759d081abcfda5d4999cf47ba
hardonCSDN <1226605334@qq.com>
2019-06-01 13:31:05 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<8){
+            System.out.println("hello git project");
+            i++;
+        }
+    }
+}
